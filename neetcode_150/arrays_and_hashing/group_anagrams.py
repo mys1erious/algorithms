af175d086bc47364b2b5d9c3bd8e44717407939a
mys1erious <subtletyzeros@gmail.com>
2022-08-31 10:58:49 +0200

    O(m*n) optimization for group_anagrams using ord()

--- neetcode_150/arrays_and_hashing/group_anagrams.py
+++ neetcode_150/arrays_and_hashing/group_anagrams.py
@@ -1,38 +1,27 @@
-def build_hashmap(s):
-    hashmap = {}
-    for c in s:
-        if c not in hashmap:
-            hashmap[c] = 1
-        else:
-            hashmap[c] += 1
-    return hashmap
+from collections import defaultdict
 
 
 def group_anagrams(strs):
-    # for each str make a hashmap where key:char val:count
-    # if hashmap not in hashmaps u add it as key:hashmap val:[str]
-    #   where hashmaps is a hashmap itself where key:frozen hashmap val:array of str anagrams
-    # return str_hashmaps.values().all() as array
-    hashmaps = {}
+    hashmap = defaultdict(list)
 
-    for i, s in enumerate(strs):
-        hashmap = frozenset(build_hashmap(s))
-        if hashmap not in hashmaps:
-            hashmaps[hashmap] = [s]
-        else:
-            hashmaps[hashmap].append(s)
+    for s in strs:
 
-    print(list(hashmaps.values()))
-    return list(hashmaps.values())
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+
+        hashmap[tuple(count)].append(s)
+
+    return hashmap.values()
 
 
 if __name__ == '__main__':
-    #strs1 = ["eat", "tea", "tan", "ate", "nat", "bat"]  # Output: [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
-    #strs2 = [""]  # Output: [[""]]
-    #strs3 = ["a"]  # Output: [["a"]]
+    strs1 = ["eat", "tea", "tan", "ate", "nat", "bat"]  # Output: [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+    strs2 = [""]  # Output: [[""]]
+    strs3 = ["a", "b"]  # Output: [["a"]]
     strs4 = ["ddddddddddg", "dgggggggggg"]
 
-    #assert group_anagrams(strs1) == [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+    assert group_anagrams(strs1) == [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
     #assert group_anagrams(strs2) == [[""]]
-    #assert group_anagrams(strs3) == [["a"]]
-    assert group_anagrams(strs4) == [["dgggggggggg"],["ddddddddddg"]]
+    #assert group_anagrams(strs3) == [["a"], ["b"]]
+    #assert group_anagrams(strs4) == [["dgggggggggg"],["ddddddddddg"]]
--- test.py
+++
@@ -1,8 +0,0 @@
-dc = {1: 'val1', 2: 'val2'}
-dcf = frozenset(dc)
-
-
-main = {dcf: 'someVal'}
-
-
-print
